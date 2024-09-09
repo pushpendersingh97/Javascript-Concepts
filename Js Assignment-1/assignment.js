cce37ec391cf4e3405ffc001ffcee19d5a185f33
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-09-09 21:42:04 +0530

    Assignment 1 Done

---
+++ Js Assignment-1/assignment.js
@@ -0,0 +1,10 @@
+let result;
+const userInput = 0;
+
+result = userInput + 18;
+result = result - 2;
+result = result * 2;
+result = result / 2;
+
+alert("Result: "+ result);
+alert("User Input: "+ userInput);
