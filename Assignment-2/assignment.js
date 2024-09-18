2e46e743b9953291d69fbb2165eb08a2e8511524
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-09-18 11:23:53 +0530

    Assignment 2

---
+++ Assignment-2/assignment.js
@@ -0,0 +1,27 @@
+const task3Element = document.getElementById('task-3');
+
+// Task 1
+function greet() {
+    alert('Hi there!');
+}
+
+// Task 2
+function greetUser(userName) {
+    alert(userName);
+}
+
+// Task 3
+// greet();
+// greetUser('Max');
+task3Element.addEventListener('click', greet);
+
+// Task 4
+function concat(str1, str2, str3) {
+    return str1 + str2 + str3;
+}
+
+// Task 5
+// const newStr = concat('Hello ', 'World', '!');
+// alert(newStr);
+
+
